6dbd26813cbd4c5f7b80ddab8080f7bd6d84c699
lux <lukicaleksa04@gmail.com>
2019-12-09 14:45:26 +0100

    db

--- src/main/java/com/example/demo/controller/AdminController.java
+++ src/main/java/com/example/demo/controller/AdminController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/admin")
+    @RequestMapping("/admin")
 public class AdminController {
 
     //region --Event --
