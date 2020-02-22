9c6e1661879d8711b570b4ec009b2232bc872143
Aleksa Lukic <lukicaleksa04@gmail.com>
2020-02-22 13:19:12 +0100

    Prepravljen servis za slanje mejla Dodati 10 treda istovrenemo

--- src/main/java/com/example/demo/service/AdminServiceImpl.java
+++ src/main/java/com/example/demo/service/AdminServiceImpl.java
@@ -3,16 +3,21 @@ package com.example.demo.service;
 import com.example.demo.dto.MailDTO;
 import com.example.demo.entity.Admin;
 import com.example.demo.repository.AdminRepository;
+import javassist.bytecode.analysis.Executor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.mail.*;
+import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Properties;
+import java.util.concurrent.*;
+import java.util.regex.Pattern;
 
 @Service
 public class AdminServiceImpl implements AdminService {
@@ -65,38 +70,60 @@ public class AdminServiceImpl implements AdminService {
     }
 
     @Override
-    public String sendMail(MailDTO mailDTO) throws MessagingException {
-        Properties props = new Properties();
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.smtp.host", "smtp.gmail.com");
-        props.put("mail.smtp.port", "587");
+    public String sendMail(MailDTO mailDTO) throws MessagingException, InterruptedException {
+        ExecutorService executor = Executors.newFixedThreadPool(4);
 
-        Session session = Session.getInstance(props, new javax.mail.Authenticator() {
-            protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication("nlpmasterakademija@gmail.com", "Preparantus");
+        int partNum = mailDTO.getMailList().size() / 10;
+        MailServiceImpl mailService;
+        MailServiceImpl mailService1;
+        MailServiceImpl mailService2;
+        MailServiceImpl mailServic3;
+        MailServiceImpl mailService4;
+        MailServiceImpl mailService5;
+        MailServiceImpl mailServic6;
+        MailServiceImpl mailService7;
+        MailServiceImpl mailService8;
+        MailServiceImpl mailService9;
+        MailServiceImpl mailService10;
+
+
+
+
+        Thread[] arrThred = {
+                mailService = new MailServiceImpl(mailDTO, 0, partNum),
+                mailService1 = new MailServiceImpl(mailDTO, partNum, 2 * partNum),
+                mailService2 = new MailServiceImpl(mailDTO, 2 * partNum, 3 * partNum),
+
+                mailServic3 = new MailServiceImpl(mailDTO, 3 * partNum, 4 * partNum),
+                mailService4 = new MailServiceImpl(mailDTO, 4 * partNum, 5 * partNum),
+                mailService5 = new MailServiceImpl(mailDTO, 5 * partNum, 6 * partNum),
+
+                mailServic6 = new MailServiceImpl(mailDTO, 6 * partNum, 7 * partNum),
+                mailService7 = new MailServiceImpl(mailDTO, 7 * partNum, 8 * partNum),
+                mailService8 = new MailServiceImpl(mailDTO, 8 * partNum, 9 * partNum),
+
+                mailService9 = new MailServiceImpl(mailDTO, 9 * partNum, 10 * partNum),
+                mailService10 = new MailServiceImpl(mailDTO, 10 * partNum, mailDTO.getMailList().size())
+        };
+
+        for (int i = 0; i < arrThred.length; i++) {
+            System.out.println("Threar: "+i);
+            if (i == 5) {
+                arrThred[i].start();
+                Thread.sleep(45000);
+            }else if(i==9){
+                arrThred[i].start();
+                Thread.sleep(10000);
             }
-        });
-
-        for (int i = 0; i < mailDTO.getMailList().size(); i++) {
-            Message msg = new MimeMessage(session);
-            msg.setFrom(new InternetAddress(mailDTO.getMailList().get(i), false));
-
-            msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(mailDTO.getMailList().get(i)));
-            msg.setSubject(mailDTO.getSubject());
-            msg.setContent(mailDTO.getMailContent(), "text/html");
-            msg.setSentDate(new Date());
-
-            MimeBodyPart messageBodyPart = new MimeBodyPart();
-            messageBodyPart.setContent("Tutorials point email", "text/html");
-
-            Transport.send(msg);
-
-
+            else {
+                arrThred[i].start();
+            }
         }
-        return "Email sent!";
 
+        return "Email sent!";
     }
+
+
 }
 
 
