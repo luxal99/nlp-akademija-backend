c7ac9b54b718af1d638cd95d8647f42303fe4b3d
Aleksa <lukicaleksa04@gmail.com>
2020-03-26 09:22:50 +0100

    Dodat DTO za client

---
+++ src/main/java/com/example/demo/dto/ClientDTO.java
@@ -0,0 +1,34 @@
+package com.example.demo.dto;
+
+public class ClientDTO {
+    private String name;
+    private String lastname;
+    private String mail;
+
+    public ClientDTO() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+}
