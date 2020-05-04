9d981759ea697d3e188aabbd7634faf102068415
Aleksa <lukicaleksa04@gmail.com>
2020-05-04 14:02:35 +0200

    Event controller

--- src/main/java/com/example/demo/controller/EventController.java
+++ src/main/java/com/example/demo/controller/EventController.java
@@ -1,6 +1,10 @@
 package com.example.demo.controller;
 
 import com.example.demo.entity.Event;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/event")
 public class EventController extends GenericController<Event> {
 }
