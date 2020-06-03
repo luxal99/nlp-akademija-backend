83705ff3276a51d6f1b8e4703fdb8d8df2dce9f3
Aleksa <lukicaleksa04@gmail.com>
2020-06-04 00:05:52 +0200

    CascadeType

--- src/main/java/com/example/demo/entity/Client.java
+++ src/main/java/com/example/demo/entity/Client.java
@@ -9,16 +9,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import java.io.Serializable;
 import java.util.List;
-import javax.persistence.Basic;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlTransient;
 
@@ -45,7 +36,7 @@ public class Client extends BaseEntity implements Serializable {
     private String telephoneNum;
     @Column(name = "date")
     private String date;
-    @OneToMany(mappedBy = "idClient")
+    @OneToMany(cascade = CascadeType.ALL,mappedBy = "idClient")
     @JsonIgnore
     private List<FlirtingMaster> flirtingMasterList;
 
--- src/main/java/com/example/demo/entity/FlirtingMaster.java
+++ src/main/java/com/example/demo/entity/FlirtingMaster.java
@@ -6,29 +6,18 @@
 package com.example.demo.entity;
 
 import java.io.Serializable;
-import javax.persistence.Basic;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.xml.bind.annotation.XmlRootElement;
 
 /**
- *
  * @author luxal
  */
 @Entity
 @Table(name = "flirting_master", catalog = "nlp_akademija", schema = "")
 @XmlRootElement
 @NamedQueries({
-    @NamedQuery(name = "FlirtingMaster.findAll", query = "SELECT f FROM FlirtingMaster f"),
-    @NamedQuery(name = "FlirtingMaster.findById", query = "SELECT f FROM FlirtingMaster f WHERE f.id = :id")})
+        @NamedQuery(name = "FlirtingMaster.findAll", query = "SELECT f FROM FlirtingMaster f"),
+        @NamedQuery(name = "FlirtingMaster.findById", query = "SELECT f FROM FlirtingMaster f WHERE f.id = :id")})
 public class FlirtingMaster extends BaseEntity implements Serializable {
 
     private static final long serialVersionUID = 1L;
